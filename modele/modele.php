bcfbe7f9bad397b9882863d878cc3cc25858bd3e
mattias_mtv <mattias.mathevon@gmail.com>
2026-03-26 11:25:10 +0100

    Fix/bug

--- modele/modele.php
+++ modele/modele.php
@@ -12,10 +12,10 @@
     return $medicaments;
   }
 
-  // Retourne une seul activités par id
+  // Retourne un seul médicament par id
   function selectMedocById($id)
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/medicament?id=" . $id;    
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/medicament.php?id=" . $id;
     $options = array(
       'http' => array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -23,14 +23,13 @@
     );
     $context = stream_context_create($options);
     $medicament = file_get_contents($url, false, $context);
-    $medicament = substr($medicament, 1); // pour supprimer le '/' du début json
     return $medicament;
   }
 
   // Retourne toutes les activites
   function selectActivite()
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/Activites";    
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/Activites.php";
     $options = array(
       'http' => array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -38,14 +37,13 @@
     );
     $context = stream_context_create($options);
     $activites = file_get_contents($url, false, $context);
-    $activites = substr($activites, 1); // pour supprimer le '/' du début json
     return $activites;
   }
 
-  // Retourne une seul activités par id
+  // Retourne une seule activité par id
   function selectActiviteById($id)
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/Activites?id=" . $id;    
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/Activites.php?id=" . $id;
     $options = array(
       'http' => array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -53,13 +51,12 @@
     );
     $context = stream_context_create($options);
     $activite = file_get_contents($url, false, $context);
-    $activite = substr($activite, 1); // pour supprimer le '/' du début json
     return $activite;
   }
 
   function selectIncription()
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/inscription.php";    
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/inscription.php";
     $options = array(
       'http' =>array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -67,15 +64,13 @@
     );
     $context = stream_context_create($options);
     $inscriptions = file_get_contents($url, false, $context);
-    $inscriptions=substr($inscriptions,1);
-    return $inscriptions ;
-
+    return $inscriptions;
   }
 
   // Retourne les effets thérapeutiques par id de médicament
   function selectEffetTherapeutiqueById($id)
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/effet_therapeutique.php?id=" . $id;
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/effet_therapeutique.php?id=" . $id;
     $options = array(
       'http' => array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -83,14 +78,13 @@
     );
     $context = stream_context_create($options);
     $effets = file_get_contents($url, false, $context);
-    $effets = substr($effets, 1); // pour supprimer le '/' du début json
     return $effets;
   }
 
   // Retourne les effets secondaires par id de médicament
   function selectEffetSecondaireById($id)
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/effet_secondaire.php?id=" . $id;
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/effet_secondaire.php?id=" . $id;
     $options = array(
       'http' => array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -98,14 +92,13 @@
     );
     $context = stream_context_create($options);
     $effets = file_get_contents($url, false, $context);
-    $effets = substr($effets, 1); // pour supprimer le '/' du début json
     return $effets;
   }
 
   // Retourne les interactions par id de médicament
   function selectInteractionById($id)
   {
-    $url = "http://127.0.0.1/ProjetApi/ProjetApi/interaction.php?id=" . $id;
+    $url = "http://127.0.0.1/ProjetApi/ProjetApi/api/interaction.php?id=" . $id;
     $options = array(
       'http' => array(
       'header' => "Content_type: application/x-www-form-urlencoded\r\n",
@@ -113,16 +106,14 @@
     );
     $context = stream_context_create($options);
     $interactions = file_get_contents($url, false, $context);
-    $interactions = substr($interactions, 1); // pour supprimer le '/' du début json
     return $interactions;
   }
 
   function insertInscription($nom, $prenom, $mail, $ville)
   {
-    $url = 'http://127.0.0.1/ProjetApi/ProjetApi/inscription.php';
+    $url = 'http://127.0.0.1/ProjetApi/ProjetApi/api/inscription.php';
     $data = array('nom' => $nom, 'prenom' => $prenom, 'mail' => $mail, 'ville' => $ville);
 
-    
     $options = array(
       'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -133,7 +124,6 @@
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
     return $result;
-
   }
 
 ?>
