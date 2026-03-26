c1b6b1a1ad6c942af126d2f9e716174ce5726b69
Sugarrats <adrien.marc.ch@gmail.com>
2026-03-26 10:18:58 +0100

    Ajout du controleur et du modéle pour Activites

--- controleur/activites_control.php
+++ controleur/activites_control.php
@@ -1,1 +1,14 @@
-ajdqlsdj,qld
+<?php
+  function getAllActivite()
+  {
+    $activites = selectActivite(); // modèle
+    include("vues/vue_activite.php");
+  }
+
+  function getOneActivite()
+  {
+    $id = $_GET["id"];
+    $activite = selectActiviteById($id); // modèle
+    include("vues/vue_activite.php");
+  }
+?>
