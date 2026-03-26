6bd9dd44c5c80056a24346c91488e0898564bb76
Rayan GUYOT <RGUYOT@SIO.LOCAL>
2026-03-26 10:29:56 +0100

    ,juj

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php
-  require_once "modele/medicament_mod.php"; 
+  require_once "modele/modele.php"; 
   require_once "controleur/medicament_control.php"; 
 
-  // cas défaut : affichage de tous
-  getAllEtu() ; //contrôleur
+if (isset($_POST["actionEtu"]))
+{
+	if ($_POST["actionEtu"]=="inscrire")
+	{
+		addEtu() ; //contrôleur
+
+	}
+	else
+		if ($_POST["actionEtu"]=="modifier")
+		{
+			updEtu() ; //contrôleur
+		}
+		else
+		{
+			//cas suppr
+			delEtu() ; //contrôleur
+		}
+}
+
+
+// cas défaut : affichage de tous
+getAllEtu() ; //contrôleur
+
+?>
 
 ?>
